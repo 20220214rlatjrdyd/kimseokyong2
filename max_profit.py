35bea0d8b6c0483db7762d9f0580d6de18e3c828
20220214rlatjrdyd <sda13400@naver.com>
2022-12-06 19:31:23 +0900

    initial state

---
+++ max_profit.py
@@ -0,0 +1,7 @@
+   for i, price in enumerate(prices):
+       for j in range(i, len(prices)):
+        max_price = max(prices[j] - price, max_price)
+	max_price = max(prices[j] - price, max_price)
+
+   return
+
